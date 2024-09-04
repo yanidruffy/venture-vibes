577e097aabde4e14264998ed02889ec813f13760
Jan Rafanan <yanidruffy@gmail.com>
2024-09-04 06:22:30 +0000

    Connect PostgreSQL database, create a simple functioning administration site and prepare for deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn venture_vibes.wsgi
--- venture_vibes/settings.py
+++ venture_vibes/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sg3h0b%w4=v^vg3gqld21y=engurj6h2@-+hf$$8^!o80y-pt8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-yanidruffy-venturevibes-7tlh497qysc.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = [
+    '8000-yanidruffy-venturevibes-7tlh497qysc.ws.codeinstitute-ide.net',
+    '.herokuapp.com'
+    ]
 
 
 # Application definition
@@ -74,13 +81,21 @@ WSGI_APPLICATION = 'venture_vibes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
