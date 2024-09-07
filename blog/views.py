7922236cc245603db9780c0a23417ae6094bcb36
Jan Rafanan <yanidruffy@gmail.com>
2024-09-07 20:39:36 +0000

    feat: Display the latest blog post on homepage

--- blog/views.py
+++ blog/views.py
@@ -14,3 +14,13 @@ class PostDetailView(generic.DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
+
+def index(request):
+    latest_post = Post.objects.filter(status=Post.Status.PUBLISHED).order_by('-publish').first()
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'latest_post': latest_post
+        },
+    )
