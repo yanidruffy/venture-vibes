95df4c42270a1bac729a892b7abeaa7dea5200ab
Jan Rafanan <yanidruffy@gmail.com>
2024-09-07 14:27:56 +0000

    feat: Add homepage URL pattern to urls.py

--- venture_vibes/urls.py
+++ venture_vibes/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('blog/', include(('blog.urls', 'blog'), namespace='blog-urls')),
 ]
