9e4ed35d5e50dfa4f2d143a0333602135596988b
Jan Rafanan <yanidruffy@gmail.com>
2024-09-09 09:45:29 +0000

    feat: Add new 'about' app with About model

---
+++ about/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class About(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+
+def __str__(self):
+    return self.title
--- venture_vibes/settings.py
+++ venture_vibes/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'blog.apps.BlogConfig',
+    'about',
 ]
 
 MIDDLEWARE = [
