a1f2285a32d4dada7ded86df54dcb5bcb11282e2
Jan Rafanan <yanidruffy@gmail.com>
2024-09-10 15:41:03 +0000

    feat: Add password reset functionality with email backend

--- venture_vibes/settings.py
+++ venture_vibes/settings.py
@@ -128,6 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
