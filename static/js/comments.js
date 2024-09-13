e97a069c410334d4f572ef3a1f31a0ef6596550d
Jan Rafanan <yanidruffy@gmail.com>
2024-09-13 21:55:42 +0000

    feat: Add comments.js for edit and delete functionality

---
+++ static/js/comments.js
@@ -0,0 +1,29 @@
+/**
+ * Code taken directly from Code Institute's 'I think therefore I blog' project
+ */
+
+const editButtons = document.getElementsByClassName("btn-edit");
+const commentText = document.getElementById("id_body");
+const commentForm = document.getElementById("commentForm");
+const submitButton = document.getElementById("submitButton");
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+for (let button of editButtons) {
+  button.addEventListener("click", (e) => {
+    let commentId = e.target.getAttribute("data-comment_id");
+    let commentContent = document.getElementById(`comment${commentId}`).innerText;
+    commentText.value = commentContent;
+    submitButton.innerText = "Update";
+    commentForm.setAttribute("action", `edit_comment/${commentId}`);
+  });
+}
+
+for (let button of deleteButtons) {
+  button.addEventListener("click", (e) => {
+    let commentId = e.target.getAttribute("data-comment_id");
+    deleteConfirm.href = `delete_comment/${commentId}`;
+    deleteModal.show();
+  });
+}
