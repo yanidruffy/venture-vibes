bd909492e74c5b88db7615a1671916fda5a5f3b1
Jan Rafanan <yanidruffy@gmail.com>
2024-09-18 11:05:24 +0000

    feat: Display liked posts on user profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from blog.models import Comment
+from blog.models import Comment, Post
 
 # Create your views here.
 
@@ -9,6 +9,7 @@ def user_profile(request):
     user = request.user
     approved_comments = Comment.objects.filter(author=user, active=True)
     unapproved_comments = Comment.objects.filter(author=user, active=False)
+    liked_posts = Post.objects.filter(likes=user)
 
     return render(
         request,
@@ -16,6 +17,7 @@ def user_profile(request):
         {
             'approved_comments': approved_comments,
             'unapproved_comments': unapproved_comments,
+            'liked_posts': liked_posts,
             'user': user,
         }
     )
