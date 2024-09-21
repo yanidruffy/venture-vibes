c7b124a5706ae05c080391b49c3b8dcdae7d5aed
Jan Rafanan <yanidruffy@gmail.com>
2024-09-21 20:17:26 +0000

    feat: Add smooth scroll to comment form when clicking on edit button

--- static/js/comments.js
+++ static/js/comments.js
@@ -14,6 +14,10 @@ for (let button of editButtons) {
     commentText.value = commentContent;
     submitButton.innerText = "Update";
     commentForm.setAttribute("action", `edit_comment/${commentId}`);
+    commentForm.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+    });
   });
 }
 
